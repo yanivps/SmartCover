4f2c305adf936b5be2ccff21eb32ba2999bb5ed5
Yaniv Pinchas <yanivps@gmail.com>
2018-01-18 21:06:26 +0200

    fixed form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
+  end
+
+  def contact_us
+    redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  match 'contact_us', to: 'welcome#contact_us', via: [:post]
 
   root 'welcome#index'
 end
