117184634baf29b1f5b012c57bcf5a5e4fbdb0c9
Yaniv Pinchas <yanivps@gmail.com>
2018-01-19 03:27:44 +0200

    escape html input before sending email

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,8 +6,8 @@ class WelcomeController < ApplicationController
 
   def contact_us
     # change to_email to smartmerge00@gmail.com
-    mail_body = "Name: #{params['name']}<br>Phone: #{params['phone']}<br>Company: #{params['company']}<br>Field: #{params['field']}<br>Comments: #{params['comments']}"
-    send_email(params['name'], params['email'], {to_email: 'yanivps123@gmail.com', bcc: 'yanivps2@gmail.com'}, "SmartCover contact request", mail_body)
+    mail_body = "Name: #{CGI::escapeHTML params['name']}<br>Phone: #{CGI::escapeHTML params['phone']}<br>Company: #{CGI::escapeHTML params['company']}<br>Field: #{CGI::escapeHTML params['field']}<br>Comments: #{CGI::escapeHTML params['comments']}"
+    send_email(CGI::escapeHTML(params['name']), CGI::escapeHTML(params['email']), {to_email: 'yanivps123@gmail.com', bcc: 'yanivps2@gmail.com'}, "SmartCover contact request", mail_body)
     flash[:contact_us_result] = "Thank you!"
     redirect_to '/'
   end
