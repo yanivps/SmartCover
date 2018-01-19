b2e3a412da9d0e3cc5199fb6d7dad2a12b509d38
Yaniv Pinchas <yanivps@gmail.com>
2018-01-19 14:19:26 +0200

    changed email to smartmerge00@gmail.com and added exception handling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,10 +5,14 @@ class WelcomeController < ApplicationController
   end
 
   def contact_us
-    # change to_email to smartmerge00@gmail.com
     mail_body = "Name: #{CGI::escapeHTML params['name']}<br>Phone: #{CGI::escapeHTML params['phone']}<br>Company: #{CGI::escapeHTML params['company']}<br>Field: #{CGI::escapeHTML params['field']}<br>Comments: #{CGI::escapeHTML params['comments']}"
-    send_email(CGI::escapeHTML(params['name']), CGI::escapeHTML(params['email']), {to_email: 'yanivps123@gmail.com', bcc: 'yanivps2@gmail.com'}, "SmartCover contact request", mail_body)
-    flash[:contact_us_result] = "Thank you!"
+    begin
+      send_email(CGI::escapeHTML(params['name']), CGI::escapeHTML(params['email']), {to_email: 'smartmerge00@gmail.com', bcc: 'yanivps2@gmail.com'}, "SmartCover contact request", mail_body)
+      flash[:contact_us_result] = "Thank you!"
+    rescue => e
+      Rails.logger.error "Failed send email with error: #{e.message}\nMessage body: Name: #{params[:name]} Email: #{params[:email]} MailBody: #{mail_body}"
+      flash[:contact_us_result] = "There was an error. Please try again later"
+    end
     redirect_to '/'
   end
 end
